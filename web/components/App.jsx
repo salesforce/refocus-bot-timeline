4100837c619a19c825f678f2e27632133cd80d97
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2018-01-10 17:00:43 -0500

    Add icons

--- web/components/App.jsx
+++ web/components/App.jsx
@@ -60,9 +60,12 @@ class App extends React.Component{
     const { response } = this.state;
     const buttonHeaderClass = 'slds-size_1-of-1 slds-text-align_center' +
       ' slds-docked-composer__header';
-
     const footerClass = 'slds-docked-composer__footer slds-grid slds-form' +
       ' slds-form_stacked slds-p-horizontal_medium slds-m-bottom_small';
+    const svgIconClass = 'slds-icon_container slds-icon-utility-change_owner ' +
+      'slds-chat-icon';
+    const iconClass = 'slds-icon slds-m-bottom_xx-small slds-icon_xx-small';
+    const iconPath = '../static/icons/utility-sprite/svg/symbols.svg';
 
     return (
       <div>
@@ -132,6 +135,13 @@ class App extends React.Component{
                     <div className="slds-chat-event">
                       <div className="slds-chat-event__rule"></div>
                       <div className="slds-chat-event__body">
+                        <span className={svgIconClass}>
+                          <svg className={iconClass} aria-hidden="true">
+                            <use
+                              xlinkHref= {iconPath + '#event'}>
+                            </use>
+                          </svg>
+                        </span>
                         <p>
                           <b>Event</b> was performed •&nbsp;
                           {moment(event.createdAt).format('YYYY-MM-DD HH:mm Z')}
@@ -141,6 +151,31 @@ class App extends React.Component{
                       <div className="slds-chat-event__agent-message">
                         {event.log}
                       </div>
+                    </div>
+                  </li>
+                );
+              }
+              if ((event.context) && (event.context.type === 'User')) {
+                return (
+                  <li
+                    className="slds-chat-listitem slds-chat-listitem_event"
+                    key={event.id}>
+                    <div className="slds-chat-event">
+                      <div className="slds-chat-event__rule"></div>
+                      <div className="slds-chat-event__body">
+                        <span className={svgIconClass}>
+                          <svg className={iconClass} aria-hidden="true">
+                            <use
+                              xlinkHref={iconPath + '#user'}>
+                            </use>
+                          </svg>
+                        </span>
+                        <p>
+                          <b>User</b> has joined room •&nbsp;
+                          {moment(event.createdAt).format('YYYY-MM-DD HH:mm Z')}
+                        </p>
+                      </div>
+                      <div className="slds-chat-event__rule"></div>
                     </div>
                   </li>
                 );
