4c633861af2dd65694dd0d54627337964af0711e
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2018-04-04 13:26:21 -0400

    Fix Duplicates

--- web/components/App.jsx
+++ web/components/App.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 const moment = require('moment');
+const _ = require('lodash');
 const React=require('react');
 const { env } = require('../../config.js');
 const config = require('../../config.js')[env];
@@ -19,8 +20,10 @@ class App extends React.Component{
       roomId: this.props.roomId,
       response: this.props.response
         .sort((a, b) => moment(a.createdAt).diff(moment(b.createdAt)))
-        .filter((value, index, self) => { 
-          return self.indexOf(value) === index;
+        .filter((value, index, self) => {
+          const dulplicates = _.filter(self.slice(0, index), ['id', value.id]);
+          return  dulplicates.length === 0 ?
+            value : false;
         }),
       currentText: '',
       scroll: false,
@@ -34,11 +37,14 @@ class App extends React.Component{
   }
 
   componentWillReceiveProps(nextProps) {
-    const eventLog = this.state.response.concat(nextProps.response);
-    eventLog.filter((value, index, self) => { 
-      return self.indexOf(value) === index;
-    });
-    
+    let eventLog = this.state.response.concat(nextProps.response);
+    eventLog = eventLog
+      .sort((a, b) => moment(a.createdAt).diff(moment(b.createdAt)))
+      .filter((value, index, self) => {
+        const dulplicates = _.filter(self.slice(0, index), ['id', value.id]);
+        return  dulplicates.length === 0 ?
+          value : false;
+      });
     this.setState({ response: eventLog });
     this.setState({ scroll: true });
   }
