f6b9feebb8bf467e71d36a055dda970b45506d9c
kriscfoster <kfoster@kfoster-wsm1.internal.salesforce.com>
2018-06-12 11:40:33 +0100

    style

--- web/components/ChatBox.jsx
+++ web/components/ChatBox.jsx
@@ -6,7 +6,6 @@ class ChatBox extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      currentText: this.props.currentText,
       chatChange: this.props.chatChange,
       sendChat: this.props.sendChat,
       pendingMessage: this.props.pendingMessage,
@@ -15,7 +14,6 @@ class ChatBox extends React.Component{
 
   componentWillReceiveProps(nextProps) {
     this.setState({
-      currentText: nextProps.currentText,
       pendingMessage: nextProps.pendingMessage
     });
 
@@ -25,7 +23,7 @@ class ChatBox extends React.Component{
   }
 
   render(){
-    const { currentText, chatChange, sendChat, pendingMessage } = this.state;
+    const { chatChange, sendChat, pendingMessage } = this.state;
     const footerClass = 'slds-docked-composer__footer slds-grid slds-form' +
       ' slds-form_stacked slds-p-horizontal_medium';
 
@@ -34,15 +32,19 @@ class ChatBox extends React.Component{
         <div className="slds-form-element slds-col">
           <div className="slds-form-element__control slds-p-around_xx-small">
             <div id="chat"
+              className="slds-input slds-rich-text-editor"
+              contentEditable="true" 
+              style={{ maxHeight: '100px', overflow: 'auto' }}
               onKeyUp={chatChange}
               placeholder="Type Message"
               onKeyPress={(event) => {
-                if ((event.key === 'Enter' && !event.shiftKey) &&
-                  (!pendingMessage)) {
-                  sendChat();
+                if (event.key === 'Enter') {
+                  if (!event.shiftKey && !pendingMessage) {
+                    event.preventDefault();
+                    sendChat();
+                  } 
                 }
-              }}
-              contentEditable="true" className="slds-input slds-rich-text-editor" style={{ maxHeight: '100px', overflow: 'auto' }}>
+              }}>
             </div>
           </div>
         </div>
@@ -63,7 +65,6 @@ class ChatBox extends React.Component{
 }
 
 ChatBox.propTypes={
-  currentText: PropTypes.string,
   chatChange: PropTypes.func,
   sendChat: PropTypes.func,
   pendingMessage: PropTypes.bool
