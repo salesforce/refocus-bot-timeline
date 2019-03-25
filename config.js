23a5fcc596eafc8db27198d18a791d46736cb4cd
Kris foster <kfoster@salesforce.com>
2019-03-25 14:25:01 +0000

    Update config.js

--- config.js
+++ config.js
@@ -20,25 +20,21 @@ module.exports = {
   port: process.env.PORT || DEFAULT_PORT,
   dev: {
     refocusUrl: process.env.REFOCUS_DEV_URL,
-    loginUrl: process.env.SFDC_URL,
     token: process.env.API_TOKEN,
     socketToken: process.env.SOCKET_TOKEN,
   },
   perf: {
     refocusUrl: process.env.REFOCUS_PERF_URL,
-    loginUrl: process.env.SFDC_URL,
     token: process.env.API_TOKEN,
     socketToken: process.env.SOCKET_TOKEN,
   },
   sandbox: {
     refocusUrl: process.env.REFOCUS_SANDBOX_URL,
-    loginUrl: process.env.SFDC_URL,
     token: process.env.API_TOKEN,
     socketToken: process.env.SOCKET_TOKEN,
   },
   production: {
     refocusUrl: process.env.REFOCUS_PRODUCTION_URL,
-    loginUrl: process.env.SFDC_URL,
     token: process.env.API_TOKEN,
     socketToken: process.env.SOCKET_TOKEN,
   },
