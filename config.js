f4a79a8d0ed5abd11f399f1897849e488d7e3bd1
Kris foster <kfoster@salesforce.com>
2019-03-25 10:49:58 +0000

    different env var

--- config.js
+++ config.js
@@ -19,28 +19,25 @@ module.exports = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || DEFAULT_PORT,
   dev: {
-    refocusUrl: process.env.REFOCUS_URL ||
-      'http://localhost:3000',
+    refocusUrl: process.env.REFOCUS_DEV_URL,
     loginUrl: process.env.SFDC_URL,
     token: process.env.API_TOKEN,
     socketToken: process.env.SOCKET_TOKEN,
   },
   perf: {
-    refocusUrl: process.env.REFOCUS_URL,
+    refocusUrl: process.env.REFOCUS_PERF_URL,
     loginUrl: process.env.SFDC_URL,
     token: process.env.API_TOKEN,
     socketToken: process.env.SOCKET_TOKEN,
   },
   sandbox: {
-    refocusUrl: process.env.REFOCUS_URL ||
-      'https://refocus-sandbox.internal.salesforce.com',
+    refocusUrl: process.env.REFOCUS_SANDBOX_URL,
     loginUrl: process.env.SFDC_URL,
     token: process.env.API_TOKEN,
     socketToken: process.env.SOCKET_TOKEN,
   },
   production: {
-    refocusUrl: process.env.REFOCUS_URL ||
-      'https://refocus.internal.salesforce.com',
+    refocusUrl: process.env.REFOCUS_PRODUCTION_URL,
     loginUrl: process.env.SFDC_URL,
     token: process.env.API_TOKEN,
     socketToken: process.env.SOCKET_TOKEN,
