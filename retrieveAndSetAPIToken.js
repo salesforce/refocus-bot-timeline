827f14733a361d4b172f832b999ae7bcef43d151
Kris Foster <kfoster@salesforce.com>
2019-04-23 15:01:17 +0100

    update

--- retrieveAndSetAPIToken.js
+++ retrieveAndSetAPIToken.js
@@ -1,14 +1,17 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const util = require('util');
+const TIME_DELAY_MS = 500;
+const SECOND_ARRAY_EL = 1;
+
 fs.readFileAsync = util.promisify(fs.readFile);
 fs.writeFileAsync = util.promisify(fs.writeFile);
 
-async function setAPIToken() {
+(async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('http://localhost:3000/tokens/new',
-  	{waitUntil: ['load', 'domcontentloaded']});
+    { waitUntil: ['load', 'domcontentloaded'] });
   await page.type('input[name="username"]', 'admin@refocus.admin');
   await page.type('input[name="password"]', 'devPassword');
   await page.click('button[type="submit"]');
@@ -17,20 +20,12 @@ async function setAPIToken() {
   const tokenName = 'test_' + d.getTime().toString();
   await page.type('input', tokenName);
   await page.click('button');
-  await page.waitFor(500);
-  const element = await page.$("#tokenInfo");
+  await page.waitFor(TIME_DELAY_MS);
+  const element = await page.$('#tokenInfo');
   const text = await (await element.getProperty('textContent')).jsonValue();
-  const apiToken = text.split('Token value: ')[1];
-  process.env.API_TOKEN = apiToken;
-  // const stringData = JSON.stringify(apiToken);
-  return fs.readFileAsync('.env', 'utf-8').then((data) => {
-  	data += `\n\nAPI_TOKEN=${apiToken}`;
-  	return data;
-  })
-  .then((data) => {
-  	return fs.writeFileAsync('.env', data, 'utf-8')
-  });
-};
-
-setAPIToken()
-	.then(() => process.exit());
+  const apiToken = text.split('Token value: ')[SECOND_ARRAY_EL];
+  let data = await fs.readFileAsync('.env', 'utf-8');
+  data += `\n\nAPI_TOKEN=${apiToken}`;
+  await fs.writeFileAsync('.env', data, 'utf-8');
+  await browser.close();
+})();
