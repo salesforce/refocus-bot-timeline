41b191517b4a00de02a52ec9cfeb5c5fcba80e42
kriscfoster <kfoster@kfoster-wsm1.internal.salesforce.com>
2019-05-01 14:35:07 +0100

    D.

--- retrieveAndSetAPIToken.js
+++ retrieveAndSetAPIToken.js
@@ -6,6 +6,7 @@ const SECOND_ARRAY_EL = 1;
 
 fs.readFileAsync = util.promisify(fs.readFile);
 fs.writeFileAsync = util.promisify(fs.writeFile);
+fs.openFileAsync = util.promisify(fs.open);
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -23,9 +24,10 @@ fs.writeFileAsync = util.promisify(fs.writeFile);
   await page.waitFor(TIME_DELAY_MS);
   const element = await page.$('#tokenInfo');
   const text = await (await element.getProperty('textContent')).jsonValue();
+  await fs.openFileAsync('.env', 'w');
   const apiToken = text.split('Token value: ')[SECOND_ARRAY_EL];
   let data = await fs.readFileAsync('.env', 'utf-8');
-  data += `\n\nAPI_TOKEN=${apiToken}`;
+  data += 'API_TOKEN=${apiToken}';
   await fs.writeFileAsync('.env', data, 'utf-8');
   await browser.close();
 })();
